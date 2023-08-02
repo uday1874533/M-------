9539ef818974288a034298fae335199221ba0331
uday1874533 <119723458+uday1874533@users.noreply.github.com>
2023-08-02 18:41:38 +0530

    a,b=map(int,input().split()) if(a!=0):     if(abs(a-b)==1 or a==1 and b==10 or a==10 and b==1):         print("Yes")     elif(a<=b):         print("No")     else:         print("No") else:         print("No")        #WOW FANTASIC SUPER MIND BELABLE'''        # 1ST PROBLEM RA OWN GA

---
+++ Edge_Checker.py
@@ -0,0 +1,12 @@
+a,b=map(int,input().split())
+if(a!=0):
+    if(abs(a-b)==1 or a==1 and b==10 or a==10 and b==1):
+        print("Yes")
+    elif(a<=b):
+        print("No")
+    else:
+        print("No")
+else:
+        print("No")
+       #WOW FANTASIC SUPER MIND BELABLE'''
+       # 1ST PROBLEM RA OWN GA 
