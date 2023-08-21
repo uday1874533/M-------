ef9aeb8f694493695cb16084f5a8aefe4cafe8f4
uday1874533 <119723458+uday1874533@users.noreply.github.com>
2023-08-21 09:58:44 +0530

    Add files via upload

---
+++ BULLET.py
@@ -0,0 +1,10 @@
+# INTERPE ECOMERCE
+
+# cook your dish here
+for i in range(int(input())):
+    x,y,z = map(int,input().split())
+    bt = y//x
+    ans = z-bt
+    if ans < 0:
+        ans = 0
+    print(ans)
